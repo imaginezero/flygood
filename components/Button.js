9aa448e70309379c41d87a4882eaa26602513481
dmbch <daniel@dmbch.net>
2020-04-09 16:23:20 +0200

    chore: tweak button styling

--- components/Button.js
+++ components/Button.js
@@ -10,7 +10,7 @@ export const Button = createStyleableComponent('button', {
     disabled ? 'cursor-not-allowed' : 'cursor-pointer',
   focus: 'focus:shadow-md',
   margin: 'mt-2',
-  padding: 'py-1 px-2',
+  padding: 'py-2 px-4',
   width: '',
   height: '',
   base: 'focus:outline-none outline-none appearance-none',
