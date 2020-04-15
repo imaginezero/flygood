abda638be6dcea3ab8aec3c52ba5e00f6f862069
dmbch <daniel@dmbch.net>
2020-04-15 11:56:50 +0200

    feat: tweak results display

--- components/TripCard.js
+++ components/TripCard.js
@@ -4,6 +4,8 @@ import { useTrip } from '../hooks';
 
 import { AirportCard } from './AirportCard';
 
+const pluralize = (count, label) => `${count} ${label}${count > 1 ? 's' : ''}`;
+
 export const TripCard = () => {
   const { trip } = useTrip();
   return (
@@ -16,28 +18,36 @@ export const TripCard = () => {
           </li>
         ))}
       </ul>
-      <h3 className="text-base font-serif text-blue-800 mb-1 mt-5">
-        Emissions
-      </h3>
+      <h3 className="text-base font-serif text-blue-800 mb-1 mt-5">Analysis</h3>
       <table className="w-full text-sm text-gray-800">
-        <tr>
-          <td>Distance</td>
-          <td>{`${trip.distance} kilometers`}</td>
-        </tr>
-        <tr>
-          <td>
-            CO<sub>2</sub> Emission
-          </td>
-          <td>{`${trip.emissions} kilograms`}</td>
-        </tr>
-        <tr>
-          <td>Trees to offset emissions</td>
-          <td>{`${trip.trees} trees`}</td>
-        </tr>
-        <tr>
-          <td>Cost to offset emissions</td>
-          <td>{`${trip.cost}€`}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Distance</td>
+            <td>
+              <b>{pluralize(trip.distance, 'kilometer')}</b>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              CO<sub>2</sub> Emission
+            </td>
+            <td>
+              <b>{pluralize(trip.emissions, 'kilogram')}</b>
+            </td>
+          </tr>
+          <tr>
+            <td>Trees to offset emissions</td>
+            <td>
+              <b>{pluralize(trip.trees, 'tree')}</b>
+            </td>
+          </tr>
+          <tr>
+            <td>Cost to offset emissions</td>
+            <td>
+              <b>{`${trip.cost}€`}</b>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </Fragment>
   );
