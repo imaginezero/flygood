9afdb7a86f4b3393f01bbe96c6c97e96b571a034
dmbch <daniel@dmbch.net>
2020-04-30 17:37:09 +0200

    feat: add payment success page

--- content/index.js
+++ content/index.js
@@ -1,3 +1,4 @@
 export { default as About } from './about.md';
 export { default as Contact } from './contact.md';
 export { default as Info } from './info.md';
+export { default as Thanks } from './thanks.md';
