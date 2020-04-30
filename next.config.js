8b687bac3b0bb63a7203af161d64d38ef608420e
dmbch <daniel@dmbch.net>
2020-04-30 17:35:57 +0200

    chore: make public Stripe key available at build time

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
+  env: {
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+  },
   poweredByHeader: false,
   target: 'serverless',
 });
