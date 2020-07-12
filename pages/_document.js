ce463b35994011d243ef27c843b253e777804ecc
dmbch <daniel@dmbch.net>
2020-07-12 22:39:45 +0200

    feat: add translation infrastructure

--- pages/_document.js
+++ pages/_document.js
@@ -1,38 +1,46 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class FlygoodDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const stripeApiKey = process.env.STRIPE_PUBLIC_KEY;
-    const trackingId = process.env.GA_MEASUREMENT_ID;
-    return { ...initialProps, stripeApiKey, trackingId };
-  }
-  render() {
-    const { stripeApiKey, trackingId } = this.props;
-    return (
-      <Html>
-        <Head>
-          <meta name="robots" content="index, follow" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          {stripeApiKey ? (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `window.stripeKey = "${stripeApiKey}";`,
-              }}
+import { withTranslation } from '../hooks';
+
+export default withTranslation(
+  class FlygoodDocument extends Document {
+    static async getInitialProps(ctx) {
+      const initialProps = await Document.getInitialProps(ctx);
+      const stripeApiKey = process.env.STRIPE_PUBLIC_KEY;
+      const trackingId = process.env.GA_MEASUREMENT_ID;
+      return { ...initialProps, stripeApiKey, trackingId };
+    }
+    render() {
+      const { stripeApiKey, trackingId, t } = this.props;
+      return (
+        <Html>
+          <Head lang={t('lang')}>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0, viewport-fit=cover"
             />
-          ) : null}
-          {trackingId ? (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `window.gaId = "${trackingId}";window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());`,
-              }}
-            />
-          ) : null}
-        </body>
-      </Html>
-    );
+            <meta name="robots" content="index, follow" />
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+            {stripeApiKey ? (
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.stripeKey = "${stripeApiKey}";`,
+                }}
+              />
+            ) : null}
+            {trackingId ? (
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.gaId = "${trackingId}";window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());`,
+                }}
+              />
+            ) : null}
+          </body>
+        </Html>
+      );
+    }
   }
-}
+);
