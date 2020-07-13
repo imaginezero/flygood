e8118760c62de882ae8aee583a770764e3f93a48
dmbch <daniel@dmbch.net>
2020-07-13 10:30:46 +0200

    feat: add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,31 @@ export default withTranslation(
               name="viewport"
               content="width=device-width, initial-scale=1.0, viewport-fit=cover"
             />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link
+              rel="mask-icon"
+              href="/safari-pinned-tab.svg"
+              color="#7da7a5"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <meta name="msapplication-TileColor" content="#e5f6f5" />
+            <meta name="theme-color" content="#e5f6f5" />
             <meta name="robots" content="index, follow" />
           </Head>
           <body>
