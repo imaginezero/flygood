1c7c05f373edea51da50306287af365af079be51
Alexander Thiel <me@alexthiel.de>
2021-01-11 19:09:27 +0100

    chore: removes disabled code

--- components/Payment/Payment.js
+++ components/Payment/Payment.js
@@ -12,34 +12,6 @@ import {
   button,
 } from './Payment.module.css';
 
-// function usePayment() {
-//   const { t, formatNumber } = useTranslation();
-//   const {
-//     trip: { cost },
-//   } = useTrip();
-//   const [recipient, setRecipient] = useState('trees');
-//   const getUrl = () => {
-//     const amount = formatNumber(cost, 2);
-//     const language = t('lang');
-//     switch (recipient) {
-//       case 'trees':
-//         return `https://donorbox.org/trees-for-lure?amount=${amount}&language=${language}`;
-//       case 'greenpeace':
-//         return `https://www.greenpeace.de/spenden?betrag=${amount}`;
-//       default:
-//         throw new Error('unknown recipient');
-//     }
-//   };
-//   return {
-//     options: {
-//       trees: t('donateTrees'),
-//       greenpeace: t('donateToGreenpeace'),
-//     },
-//     value: recipient,
-//     onChange: setRecipient,
-//     href: getUrl(),
-//   };
-// }
 
 export default function Payment() {
   const { t, formatNumber } = useTranslation();
