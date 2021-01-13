ec956621cd635f5f7f90de11c386b79eca304d05
Alexander Thiel <me@alexthiel.de>
2021-01-13 14:41:22 +0100

    chore: removes unused comp

--- components/ConditionalWrapper.js
+++
@@ -1,3 +0,0 @@
-// Thanks to https://blog.hackages.io/conditionally-wrap-an-element-in-react-a8b9a47fab2
-const ConditionalWrapper = ({ condition, wrapper, children }) => 
-  condition ? wrapper(children) : children;
